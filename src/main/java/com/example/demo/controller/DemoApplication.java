f25a2aa537112d81d6b782c29e7cbf6b8e9d7957
yeongeun seo <wminec@gmail.com>
2025-08-25 02:13:39 +0000

    elijfew

---
+++ src/main/java/com/example/demo/controller/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "hello, Java17 Spring 3.5.6 world!";
+    }
+
+}
